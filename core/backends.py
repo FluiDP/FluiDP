4f93569a657bee72d51d11bf83b3c6eb2f674f12
SebastiaoSoares <sebastiaosoares145@gmail.com>
2026-02-03 06:34:40 -0300

    feat: Refactor authentication logic to improve user lookup and handling

--- core/backends.py
+++ core/backends.py
@@ -5,17 +5,33 @@ from django.db.models import Q
 UserModel = get_user_model() 
 
 class MatriculaBackend(ModelBackend):
+    """
+    Backend de autenticação que permite login por Username, Matrícula, CPF ou E-mail.
+    """
     
     def authenticate(self, request, username=None, password=None, **kwargs):
-        
-        try:
-            user = UserModel.objects.get(
-                Q(username__iexact=username) | Q(matricula__iexact=username) | Q(cpf__iexact=username) | Q(email__iexact=username)
-            )
+        if username is None:
+            return None
+            
+        try:            
+            query = Q(username__iexact=username) | \
+                    Q(matricula__iexact=username) | \
+                    Q(cpf__iexact=username) | \
+                    Q(email__iexact=username)
+
+            if username.isdigit():
+                clean_username = str(int(username))
+                
+                paddings = [clean_username.zfill(i) for i in range(len(clean_username), 11)]
+                
+                query |= Q(matricula__in=paddings) | Q(username__in=paddings)
+
+            user = UserModel.objects.filter(query).first()
+
         except UserModel.DoesNotExist:
             return None
         
-        if user.check_password(password):
+        if user and user.check_password(password) and self.user_can_authenticate(user):
             return user
         
         return None
